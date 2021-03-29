776ddb978f5e75e3a13103fe65bf462dea114b78
tishalewis8707 <tishalewis8707@gmail.com>
2021-03-29 02:32:33 -0400

    completed assignment

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # tishalewis8707.github.io
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>My Website</title>
+  </head>
+  <body>
+<   h1>Hello World!</h1>
+  </body>
+</html>
---
+++ script.js
@@ -0,0 +1,15 @@
+<html lang="en">
+  <head>
+    <title>My Website</title>
+  </head>
+  <body>
+<   h1>Hello World!</h1>
+  </body>
+</html>
+
+
+  let h2 = document.createElement('h2')
+  h2.textContent = "This content added by JavaScript"
+  document.querySelector('body').appendChild(h2)
+ 
+  <script src="script.js"></script>
